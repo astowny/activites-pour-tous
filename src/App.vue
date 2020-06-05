8ec919128145bb59f89f84acc021f987a21c5caf
Tony DUONG <astowny@gmail.com>
2020-06-05 22:56:51 +0200

    notes added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
 	<div id="app">
+		<!-- h1 avec image en fond -->
 		<h1>
 			BESOIN DE MOTIVATION POUR ATTEINDRE VOS OBJECTIFS ?
 			<br />LES ACTIVITES PHYSIQUES POUR TOUS VOUS Y AIDERONT
 		</h1>
+		<!-- subh1 -->
 		<p>Que vous soyez sportif, débutant ou non vous pouvez le faire !</p>
+		<!-- cartes pas trop grandes avec texte + img -->
 		<p>
 			Echauffement, mobilisation des articulations inspirés du taïchi, qi gong, pilates et yoga
 			Renforcement musculaire progressif avec ou sans matériel (cuisses-abdo-fessiers, gainage)
 			Travail d’équilibre, de coordination et de mémoire
 			Cardio adaptée en fonction de votre niveau
 			Etirement, souplesse, respiration, détente, relaxation, méditation, libération de stress et bien être.
+		</p>
+		<!-- background orange -->
+		<p>
 			PRENEZ VOTRE SANTE EN MAIN EN BENEFICIANT DES BIENTFAITS DE L’ACTIVITE PHYSIQUE ET EN DECOUVRANT VOS CAPACITES SPORTIVES.
 			GYM D’ENTRETIEN POUR MAINTENIR VOTRE FORME ET VOTRE CONDITION PHYSIQUE BASEE SUR UN PROGRAMME PERSONNALISE ET ADAPTE.
 			VOTRE COACH SPORTIF DIPLÔMEE D’ETAT SE DEPLACE CHEZ VOUS POUR VOUS PROPOSER DIFFERENTES PRESTATIONS DE SERVICES PERSONNALISEES SELON VOS OBJECTIFS.
+		</p>
+		<p>
 			COURS INDIVIDUEL OU EN GROUPE, EN INTERIEUR OU EN EXTERIEUR (EN FONCTION DE LA METEO ET DES CONTRAINTES SANITAIRES) .
 			TARIFS :
 			A PARTIR DE 6 PERSONNES : 5€/séance/personne.
