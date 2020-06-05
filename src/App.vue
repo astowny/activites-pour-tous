a0d68d920d959965b8acaec738c99553922a67e9
Tony DUONG <astowny@gmail.com>
2020-06-05 22:39:10 +0200

    rm helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>
+			BESOIN DE MOTIVATION POUR ATTEINDRE VOS OBJECTIFS ?
+			<br />LES ACTIVITES PHYSIQUES POUR TOUS VOUS Y AIDERONT
+		</h1>
+		<p>Que vous soyez sportif, débutant ou non vous pouvez le faire !</p>
+		<p>
+			Echauffement, mobilisation des articulations inspirés du taïchi, qi gong, pilates et yoga
+			Renforcement musculaire progressif avec ou sans matériel (cuisses-abdo-fessiers, gainage)
+			Travail d’équilibre, de coordination et de mémoire
+			Cardio adaptée en fonction de votre niveau
+			Etirement, souplesse, respiration, détente, relaxation, méditation, libération de stress et bien être.
+			PRENEZ VOTRE SANTE EN MAIN EN BENEFICIANT DES BIENTFAITS DE L’ACTIVITE PHYSIQUE ET EN DECOUVRANT VOS CAPACITES SPORTIVES.
+			GYM D’ENTRETIEN POUR MAINTENIR VOTRE FORME ET VOTRE CONDITION PHYSIQUE BASEE SUR UN PROGRAMME PERSONNALISE ET ADAPTE.
+			VOTRE COACH SPORTIF DIPLÔMEE D’ETAT SE DEPLACE CHEZ VOUS POUR VOUS PROPOSER DIFFERENTES PRESTATIONS DE SERVICES PERSONNALISEES SELON VOS OBJECTIFS.
+			COURS INDIVIDUEL OU EN GROUPE, EN INTERIEUR OU EN EXTERIEUR (EN FONCTION DE LA METEO ET DES CONTRAINTES SANITAIRES) .
+			TARIFS :
+			A PARTIR DE 6 PERSONNES : 5€/séance/personne.
+			De 3 à 5 PERSONNES : 10€/séance/personne.
+			COURS PARTICULIERS : 30€ /séance de 1H 270€ les 10 séances.
+			N.B. : déduction fiscale 50%, au titre des prestations de services
+			Contactez Thi Chap (Katy)
+			Educatrice Sportive diplômée d’état fitness et APT (Activités Physiques pour Tous)
+			06 52 59 18 49
+			thichaptran@gmail.com
+		</p>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
