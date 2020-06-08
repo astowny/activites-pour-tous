dd5dbef2cc9bda64c95935b748a8c4829c10243e
Tony DUONG <astowny@gmail.com>
2020-06-08 22:43:37 +0200

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,24 @@
 <template>
-	<div id="app">
-		<!-- h1 avec image en fond -->
-		<h1>
-			BESOIN DE MOTIVATION POUR ATTEINDRE VOS OBJECTIFS ?
-			<br />LES ACTIVITES PHYSIQUES POUR TOUS VOUS Y AIDERONT
-		</h1>
-		<!-- subh1 -->
-		<p>Que vous soyez sportif, débutant ou non vous pouvez le faire !</p>
-		<!-- cartes pas trop grandes avec texte + img -->
-		<p>
-			Echauffement, mobilisation des articulations inspirés du taïchi, qi gong, pilates et yoga
-			Renforcement musculaire progressif avec ou sans matériel (cuisses-abdo-fessiers, gainage)
-			Travail d’équilibre, de coordination et de mémoire
-			Cardio adaptée en fonction de votre niveau
-			Etirement, souplesse, respiration, détente, relaxation, méditation, libération de stress et bien être.
-		</p>
-		<!-- background orange -->
-		<p>
-			PRENEZ VOTRE SANTE EN MAIN EN BENEFICIANT DES BIENTFAITS DE L’ACTIVITE PHYSIQUE ET EN DECOUVRANT VOS CAPACITES SPORTIVES.
-			GYM D’ENTRETIEN POUR MAINTENIR VOTRE FORME ET VOTRE CONDITION PHYSIQUE BASEE SUR UN PROGRAMME PERSONNALISE ET ADAPTE.
-			VOTRE COACH SPORTIF DIPLÔMEE D’ETAT SE DEPLACE CHEZ VOUS POUR VOUS PROPOSER DIFFERENTES PRESTATIONS DE SERVICES PERSONNALISEES SELON VOS OBJECTIFS.
-		</p>
-		<p>
-			COURS INDIVIDUEL OU EN GROUPE, EN INTERIEUR OU EN EXTERIEUR (EN FONCTION DE LA METEO ET DES CONTRAINTES SANITAIRES) .
-			TARIFS :
-			A PARTIR DE 6 PERSONNES : 5€/séance/personne.
-			De 3 à 5 PERSONNES : 10€/séance/personne.
-			COURS PARTICULIERS : 30€ /séance de 1H 270€ les 10 séances.
-			N.B. : déduction fiscale 50%, au titre des prestations de services
-			Contactez Thi Chap (Katy)
-			Educatrice Sportive diplômée d’état fitness et APT (Activités Physiques pour Tous)
-			06 52 59 18 49
-			thichaptran@gmail.com
-		</p>
-	</div>
+	<v-app>
+		<v-app-bar app color="primary" dark></v-app-bar>
+
+		<v-content>
+			<v-card>
+				<v-card-title>La carte</v-card-title>
+				<v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quaerat deleniti, corrupti ea id reprehenderit libero, ducimus sunt ab nulla provident, consectetur repellat tempore quia. Consequatur non recusandae ipsam maiores!</v-card-text>
+			</v-card>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
 export default {
 	name: "App",
-	components: {}
+
+	components: {},
+
+	data: () => ({
+		//
+	})
 };
 </script>
-
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
